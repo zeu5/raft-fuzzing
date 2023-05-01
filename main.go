c401f6de2f061bf38e41f43cc7cdedb47ccf9c76
Srinidhi N <srinidhin@outlook.com>
2023-05-01 17:26:46 +0200

    Updated command structure

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ func FuzzCommand() *cobra.Command {
 				Steps:      horizon,
 				Strategy:   NewRandomStrategy(),
 				Guider:     NewTLCStateGuider("127.0.0.1:2023", "traces", true),
-				Mutator:    NewScaleUpIntChoiceMutator(5, 20),
+				Mutator:    NewScaleUpIntChoiceMutator(5, 10),
 				RaftEnvironmentConfig: RaftEnvironmentConfig{
 					Replicas:      replicas,
 					ElectionTick:  10,
@@ -54,9 +54,28 @@ func FuzzCommand() *cobra.Command {
 
 func OneCommand() *cobra.Command {
 	return &cobra.Command{
-		Use: "one",
+		Use: "compare",
 		Run: func(cmd *cobra.Command, args []string) {
-			CompareMutations(episodes, horizon, saveFile, replicas, requests)
+			c := NewComparision(saveFile, &FuzzerConfig{
+				Iterations: episodes,
+				Steps:      horizon,
+				Strategy:   NewRandomStrategy(),
+				Mutator:    &EmptyMutator{},
+				RaftEnvironmentConfig: RaftEnvironmentConfig{
+					Replicas:      replicas,
+					ElectionTick:  20,
+					HeartbeatTick: 2,
+					NumRequests:   requests,
+				},
+				MutPerTrace: 3,
+			})
+			c.AddGuider("tlcstate", NewTLCStateGuider("127.0.0.1:2023", "traces", false))
+			c.AddMutator("random", &EmptyMutator{})
+			c.AddMutator("scaleUpInt", NewScaleUpIntChoiceMutator(5, 20))
+			c.AddMutator("swapNodes", NewSwapNodeMutator(30))
+			c.AddMutator("swapNodes_scaleUpInt", CombineMutators(NewScaleUpIntChoiceMutator(5, 20), NewSwapNodeMutator(30)))
+
+			c.Run()
 		},
 	}
 }
